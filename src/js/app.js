f01491b49e0998c9a96fee0ac87d2b153def8ef0
hejin <hejin82@gmail.com>
2020-06-26 21:37:25 +0900

    add js files

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+import add from './modules/add';
+import tax from './modules/tax';
+
+const item1Price = 400;
+const itemsPrice = 600;
+const totalPrice = add(item1Price, itemsPrice);
+const salesTax = 1.08;
+const priceIncludeTax = tax(totalPrice, salesTax);
+
+$('body').text(priceIncludeTax);
